4841c1e4a90c37b755d516770997273ba183adaf
penguin <slomogurl37@gmail.com>
2025-04-22 00:09:41 +0200

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,67 @@
+use whoami;
+use reqwest;
+use std::fs::{self, File};
+use std::collections::HashMap;
+use tokio::net::TcpListener;
+use tokio_tungstenite::{accept_async, tungstenite::protocol::Message};
+use futures_util::{StreamExt,SinkExt};
+
+async fn get_paths() -> (String, String) {
+    let user = whoami::username();
+
+    let executor_path = format!("C:/Users/{user}/AppData/Local/ui/workspace");
+    let response = reqwest::get("https://clientsettings.roblox.com/v1/client-version/WindowsPlayer")
+        .await.unwrap()
+        .json::<HashMap<String, String>>()
+        .await.unwrap();
+
+    let current_version = response.get("clientVersionUpload").unwrap();
+    let roblox_path = format!("C:/Program Files (x86)/Roblox/Versions/{current_version}/content");
+
+    if fs::metadata(&roblox_path).is_err() {
+        panic!("Roblox installation not found, are you maybe on the wrong version?");
+    }
+
+    if fs::metadata(&executor_path).is_err() {
+        panic!("Executor path not found");
+    }
+
+    (roblox_path, executor_path)
+}
+
+#[tokio::main]
+async fn main() {
+    let (roblox_path, executor_path) = get_paths().await;
+
+    let listener = TcpListener::bind("127.0.0.1:6969").await.unwrap();
+    println!("Listening on ws://127.0.0.1:6969");
+
+    while let Ok((stream, _)) = listener.accept().await {
+        let ws_stream = accept_async(stream).await.unwrap();
+        let (mut write, mut read) = ws_stream.split();
+
+        while let Some(Ok(msg)) = read.next().await {
+            if msg.is_text() {
+                let executor_file = format!("{executor_path}/{msg}");
+
+                match fs::metadata(&executor_file) {
+                    Ok(_) => {
+                        let destination = format!("{roblox_path}/{}", msg);
+                        match fs::copy(&executor_file, &destination) {
+                            Ok(_) => {
+
+                                let asset = format!("rbxasset://{msg}");
+                                println!("Successfully moved: {msg}");
+                                write.send(Message::Text(asset)).await.unwrap()
+                            },
+                            Err(e) => eprintln!("Failed to copy executor file: {}", e),
+                        }
+                    },
+                    Err(e) => {
+                        eprintln!("Executor file does not exist or cannot be accessed: {}", e);
+                    }
+                }
+            }
+        }
+    }
+}
